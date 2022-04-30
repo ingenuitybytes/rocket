8731e87cfde06f13c9386218206c530ad02a0968
Alois Zimmermann <alois.zimmermann2003@gmail.com>
2022-04-30 22:34:18 +0200

    update output

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <iostream>
 #include "pico/stdlib.h"
 
 #define GPIO_ON 1
 #define GPIO_OFF 0
 #define LED_PIN 25
+
+using namespace std;
 
 int main() {
     stdio_init_all();
@@ -11,7 +14,8 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
     
     while (true) {
-        printf("Hello, world!\n");
+        cout << "hi" << endl;
+//        printf("Hello, world!\n");
         gpio_put(LED_PIN, GPIO_ON);
         sleep_ms(500);
         gpio_put(LED_PIN, GPIO_OFF);
