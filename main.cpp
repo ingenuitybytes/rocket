cc8c24ea42d2d78c65c5d84c8c4f47a9a0a6fa9d
Alois Zimmermann <alois.zimmermann2003@gmail.com>
2022-05-02 22:08:35 +0200

    edited main.cpp

--- main.cpp
+++ main.cpp
@@ -14,11 +14,11 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
     
     while (true) {
-        cout << "hi" << endl;
+        cout << "ping" << endl;
 //        printf("Hello, world!\n");
-        gpio_put(LED_PIN, GPIO_ON);
-        sleep_ms(500);
-        gpio_put(LED_PIN, GPIO_OFF);
+//        gpio_put(LED_PIN, GPIO_ON);
+//        sleep_ms(500);
+//        gpio_put(LED_PIN, GPIO_OFF);
         sleep_ms(500);
     }
     return 0;
