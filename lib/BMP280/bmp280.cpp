844c85ba35d56a2652a470427583724cef793549
Niklas <niklas.stephan12345@gmail.com>
2022-06-25 14:26:50 +0200

    createheaderfiles

--- lib/BMP280/bmp280.cpp
+++ lib/BMP280/bmp280.cpp
@@ -5,10 +5,10 @@
  **/
 
 #include <stdio.h>
-
 #include "hardware/i2c.h"
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
+#include "bmp280.h"
 
  /* Example code to talk to a BMP280 temperature and pressure sensor
 
@@ -27,69 +27,6 @@
     3.3v (pin 36) -> VCC on BMP280 board
     GND (pin 38)  -> GND on BMP280 board
  */
-
- // device has default bus address of 0x76
-#define ADDR _u(0x76)
-
-// hardware registers
-#define REG_CONFIG _u(0xF5)
-#define REG_CTRL_MEAS _u(0xF4)
-#define REG_RESET _u(0xE0)
-
-#define REG_TEMP_XLSB _u(0xFC)
-#define REG_TEMP_LSB _u(0xFB)
-#define REG_TEMP_MSB _u(0xFA)
-
-#define REG_PRESSURE_XLSB _u(0xF9)
-#define REG_PRESSURE_LSB _u(0xF8)
-#define REG_PRESSURE_MSB _u(0xF7)
-
-// calibration registers
-#define REG_DIG_T1_LSB _u(0x88)
-#define REG_DIG_T1_MSB _u(0x89)
-#define REG_DIG_T2_LSB _u(0x8A)
-#define REG_DIG_T2_MSB _u(0x8B)
-#define REG_DIG_T3_LSB _u(0x8C)
-#define REG_DIG_T3_MSB _u(0x8D)
-#define REG_DIG_P1_LSB _u(0x8E)
-#define REG_DIG_P1_MSB _u(0x8F)
-#define REG_DIG_P2_LSB _u(0x90)
-#define REG_DIG_P2_MSB _u(0x91)
-#define REG_DIG_P3_LSB _u(0x92)
-#define REG_DIG_P3_MSB _u(0x93)
-#define REG_DIG_P4_LSB _u(0x94)
-#define REG_DIG_P4_MSB _u(0x95)
-#define REG_DIG_P5_LSB _u(0x96)
-#define REG_DIG_P5_MSB _u(0x97)
-#define REG_DIG_P6_LSB _u(0x98)
-#define REG_DIG_P6_MSB _u(0x99)
-#define REG_DIG_P7_LSB _u(0x9A)
-#define REG_DIG_P7_MSB _u(0x9B)
-#define REG_DIG_P8_LSB _u(0x9C)
-#define REG_DIG_P8_MSB _u(0x9D)
-#define REG_DIG_P9_LSB _u(0x9E)
-#define REG_DIG_P9_MSB _u(0x9F)
-
-// number of calibration registers to be read
-#define NUM_CALIB_PARAMS 24
-
-struct bmp280_calib_param {
-    // temperature params
-    uint16_t dig_t1;
-    int16_t dig_t2;
-    int16_t dig_t3;
-
-    // pressure params
-    uint16_t dig_p1;
-    int16_t dig_p2;
-    int16_t dig_p3;
-    int16_t dig_p4;
-    int16_t dig_p5;
-    int16_t dig_p6;
-    int16_t dig_p7;
-    int16_t dig_p8;
-    int16_t dig_p9;
-};
 
 #ifdef i2c_default
 void bmp280_init() {
@@ -207,48 +144,3 @@ void bmp280_get_calib_params(struct bmp280_calib_param* params) {
 }
 
 #endif
-
-int main() {
-    stdio_init_all();
-
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-    #warning i2c / bmp280_i2c example requires a board with I2C pins
-        puts("Default I2C pins were not defined");
-#else
-    // useful information for picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
-    bi_decl(bi_program_description("BMP280 I2C example for the Raspberry Pi Pico"));
-
-    printf("Hello, BMP280! Reading temperaure and pressure values from sensor...\n");
-
-    // I2C is "open drain", pull ups to keep signal high when no data is being sent
-    i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-
-    // configure BMP280
-    bmp280_init();
-
-    // retrieve fixed compensation params
-    struct bmp280_calib_param params;
-    bmp280_get_calib_params(&params);
-
-    int32_t raw_temperature;
-    int32_t raw_pressure;
-
-    sleep_ms(250); // sleep so that data polling and register update don't collide
-    while (1) {
-        bmp280_read_raw(&raw_temperature, &raw_pressure);
-        int32_t temperature = bmp280_convert_temp(raw_temperature, &params);
-        int32_t pressure = bmp280_convert_pressure(raw_pressure, raw_temperature, &params);
-        printf("Pressure = %.3f kPa\n", pressure / 1000.f);
-        printf("Temp. = %.2f C\n", temperature / 100.f);
-        // poll every 500ms
-        sleep_ms(500);
-    }
-
-#endif
-    return 0;
-}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
+// Example Code
 #include <stdio.h>
-#include <iostream>
 #include "pico/stdlib.h"
+#include "./lib/BMP280/bmp280.h"
+
+int main() {
+    stdio_init_all();
+
+#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+    #warning i2c / bmp280_i2c example requires a board with I2C pins
+        puts("Default I2C pins were not defined");
+#else
+    // useful information for picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    bi_decl(bi_program_description("BMP280 I2C example for the Raspberry Pi Pico"));
+
+    printf("Hello, BMP280! Reading temperaure and pressure values from sensor...\n");
+
+    // I2C is "open drain", pull ups to keep signal high when no data is being sent
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+
+    // configure BMP280
+    bmp280_init();
+
+    // retrieve fixed compensation params
+    struct bmp280_calib_param params;
+    bmp280_get_calib_params(&params);
+
+    int32_t raw_temperature;
+    int32_t raw_pressure;
+
+    sleep_ms(250); // sleep so that data polling and register update don't collide
+    while (1) {
+        bmp280_read_raw(&raw_temperature, &raw_pressure);
+        int32_t temperature = bmp280_convert_temp(raw_temperature, &params);
+        int32_t pressure = bmp280_convert_pressure(raw_pressure, raw_temperature, &params);
+        printf("Pressure = %.3f kPa\n", pressure / 1000.f);
+        printf("Temp. = %.2f C\n", temperature / 100.f);
+        // poll every 500ms
+        sleep_ms(500);
+    }
+
+#endif
+    return 0;
+}
+
+
+
+
+
+//#include <stdio.h>
+//#include <iostream>
+//#include "pico/stdlib.h"
 
 /*
 States: IDLE, Tracking, STOP
@@ -14,12 +68,8 @@ STOP: write Data to SD-Card when Stop Tracking-Command is received
 //enum state {IDLE, Tracking, STOP};
 */
 
-using namespace std;
+//using namespace std;
 
-int main() {
-    stdio_init_all();
-
-    printf("Hello");
 
     
     /*
@@ -45,6 +95,3 @@ int main() {
     
 
     */
-    return 0;
-}
-
