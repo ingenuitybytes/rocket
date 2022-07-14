1f14967cb2d3cb41e8051e601b17dae257b2aad3
Nik <niklas.stephan12345@gmail.com>
2022-07-14 22:56:04 +0200

    Created CMake-Libraries(BMP280/MPU6050)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     stdio_init_all();
-
+/*
 #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
     #warning i2c / bmp280_i2c example requires a board with I2C pins
         puts("Default I2C pins were not defined");
@@ -45,6 +45,8 @@ int main() {
     }
 
 #endif
+*/
+printf("Hello World");
     return 0;
 }
 
