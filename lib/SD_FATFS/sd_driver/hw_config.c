5d13bd8d1cd017af7f9fa453a971ff1b2907bca9
Nik <niklas.stephan12345@gmail.com>
2022-09-10 15:54:40 +0200

    Work on SPI multiple slaves issue

--- lib/SD_FATFS/sd_driver/hw_config.c
+++ lib/SD_FATFS/sd_driver/hw_config.c
@@ -64,9 +64,9 @@ static sd_card_t sd_cards[] = {  // One for each SD card
         .pcName = "0:",           // Name used to mount device
         .spi = &spis[0],          // Pointer to the SPI driving this card
         .ss_gpio = 17,            // The SPI slave select GPIO for this SD card
-        .use_card_detect = true,
+        .use_card_detect = false,
         .card_detect_gpio = 22,   // Card detect
-        .card_detected_true = 1,  // What the GPIO read returns when a card is
+        .card_detected_true = -1,  // What the GPIO read returns when a card is
                                   // present. Use -1 if there is no card detect.
         .m_Status = STA_NOINIT
     }
